19b2a06548739dd26b139fccf21ad9d81f0146df
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 18:08:19 +0530

    Email Address

--- src/Com.Bridgelabz/RegularExpression.java
+++ src/Com.Bridgelabz/RegularExpression.java
@@ -4,20 +4,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression{
-    public static void lastName(){
+   // Uc3 = As a User need to enter a valid email
+  //E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) wit precise @ and . positions
+  public static void validEmail(){
 
-        Pattern pattern = Pattern.compile("^[A-Z]\\w{3,}$");
+    Pattern pattern = Pattern.compile("^[a-z\\d]{1,}[@][a-z\\d]{1,}[.]com$");
 
-        Matcher matcher = pattern.matcher("Nehere");
+    Matcher matcher = pattern.matcher("maheshnehere93@gmail.com");
       if (matcher.find() == true) {
-          System.out.println("Valid Last Name.");
+          System.out.println("Email is Valid");
       }else{
-          System.out.println("Invalid Last Name");
+          System.out.println("Email is Invalid");
       }
   }
 
-  public static void main(String[] args) {
-      
-      lastName();
-  }
+public static void main(String[] args) {
+    
+    validEmail();
+}
 }
