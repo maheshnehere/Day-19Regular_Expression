a9f9ac1fa4b5939d0512180ea469fca6575da9da
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 18:10:22 +0530

    Phone Number

--- src/Com.Bridgelabz/RegularExpression.java
+++ src/Com.Bridgelabz/RegularExpression.java
@@ -4,22 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression{
-   // Uc3 = As a User need to enter a valid email
-  //E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) wit precise @ and . positions
-  public static void validEmail(){
+    // Uc4 = As a User need to follow pre -define Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10 digit number
+  public static void phoneNumber(){
+    Pattern pattern = Pattern.compile("^[+]\\d{2} \\d{10}$");
 
-    Pattern pattern = Pattern.compile("^[a-z\\d]{1,}[@][a-z\\d]{1,}[.]com$");
-
-    Matcher matcher = pattern.matcher("maheshnehere93@gmail.com");
-      if (matcher.find() == true) {
-          System.out.println("Email is Valid");
-      }else{
-          System.out.println("Email is Invalid");
-      }
-  }
+    Matcher matcher = pattern.matcher("+91 7020249009");
+    if (matcher.find() == true) {
+        System.out.println("Phone Number is Valid");
+    }else{
+        System.out.println("Phone Number is Invalid");
+    }
+}
 
 public static void main(String[] args) {
     
-    validEmail();
+    phoneNumber();
 }
 }
